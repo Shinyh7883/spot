3f58d8748be06ccee920a961537a9c256785f5fa
Shinyh7883 <cosmos7883@gmail.com>
2021-07-07 15:25:23 +0900

    펄스제어

--- spot_functions.py
+++ spot_functions.py
@@ -9,7 +9,14 @@ class leg:
     i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
     kit = list()
     kit.append(ServoKit(channels=16, i2c=i2c_bus0, address=0x40))
+    kit[-1].frequency = 330 #주파수 설정
     val_list = [60, 60, 60, 60, 20, 20, 20, 20, 90, 90, 90, 90]
+    
+    servos = list()
+    for i in range(len(val_list)):
+        servos.append(servo.Servo(kit[0].channels[i], min_pulse=100, max_pulse=200))
+    
+    
     
     def front_R(self, theta):
         num=[10, 6, 2]
