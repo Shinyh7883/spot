70bdc37d951cf805bdd7bd915556578f599d64cb
shinyh7883 <cosmos7883@gmail.com>
2021-08-19 17:22:18 +0900

    무게중심이동 알고리즘 만들장,,

---
+++ src/cg_test.py
@@ -0,0 +1,56 @@
+from spot_mini_functions import *
+motion = motion()
+functions = functions()
+control = control()
+
+run = 1
+
+dot = [65, 50, 170]
+commend = [[dot, dot, 2],[dot, dot, 2],[dot, dot, 2],[dot, dot, 2]]
+
+while (1):
+    motion.body_move([-20, 0], commend)
+    walksp = 400
+    front_R = functions.dot_move('z', -20, commend[0][0])
+    control.commend(commend, "front_R", front_R, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend)
+
+    front_R = functions.dot_move('z', 20, commend[0][0])
+    control.commend(commend, "front_R", front_R, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend)
+
+    front_L = functions.dot_move('z', -20, commend[1][0])
+    control.commend(commend, "front_L", front_L, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend)
+
+    front_L = functions.dot_move('z', 20, commend[1][0])
+    control.commend(commend, "front_L", front_L, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend) 
+    
+    motion.body_move([40, 0], commend)
+
+    back_R = functions.dot_move('z', -20, commend[2][0])
+    control.commend(commend, "back_R", back_R, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend)
+
+    back_R = functions.dot_move('z', 20, commend[2][0])
+    control.commend(commend, "back_R", back_R, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend) 
+
+    back_L = functions.dot_move('z', -20, commend[3][0])
+    control.commend(commend, "back_L", back_L, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend) 
+
+    back_L = functions.dot_move('z', 20, commend[3][0])
+    control.commend(commend, "back_L", back_L, "linear")
+    control.commend_run(commend, 100)
+    control.commend_set(commend) 
+
+    motion.body_move([-20, 0], commend)
